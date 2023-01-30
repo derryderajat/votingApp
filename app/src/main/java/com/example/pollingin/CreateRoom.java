cf9a0b0d01a033cb5e8055c464e2e15b5129c11f
derry derajat <derryderajat@mail.com>
2023-01-31 00:13:00 +0700

    1. Activit_create_room (Add layout, Add button function untuk memindahkan layout ke displayKeyRoom)

--- app/src/main/java/com/example/pollingin/CreateRoom.java
+++ app/src/main/java/com/example/pollingin/CreateRoom.java
@@ -2,7 +2,11 @@ package com.example.pollingin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class CreateRoom extends AppCompatActivity {
 
@@ -10,5 +14,19 @@ public class CreateRoom extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_room);
+        Button onCreate =(Button) findViewById(R.id.createRoomButton);
+
+        onCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    startActivity(new Intent(CreateRoom.this,DisplayKeyRoom.class));
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Log.e("intent error",e.getMessage());
+                }
+            }
+        });
+
     }
 }
